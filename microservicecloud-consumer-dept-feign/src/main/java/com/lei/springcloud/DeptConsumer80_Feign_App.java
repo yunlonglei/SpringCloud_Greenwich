72e8a4180ed549c6f664a426148a800fb6ff6789
leiyunlong <1175333149@qq.com>
2019-07-23 15:07:53 +0800

    fegin的部分实现

--- microservicecloud-consumer-dept-feign/src/main/java/com/lei/springcloud/DeptConsumer80_Feign_App.java
+++ microservicecloud-consumer-dept-feign/src/main/java/com/lei/springcloud/DeptConsumer80_Feign_App.java
@@ -3,12 +3,14 @@ package com.lei.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"com.lei.springcloud"})
 @ComponentScan("com.lei.springcloud")
 @EnableCircuitBreaker
