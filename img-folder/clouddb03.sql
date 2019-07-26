295cd7bae5a7fc54442ddb47c6c08bca699bf725
leiyunlong <1175333149@qq.com>
2019-07-26 16:50:39 +0800

    数据库文件转储

---
+++ img-folder/clouddb03.sql
@@ -0,0 +1,40 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : my
+ Source Server Type    : MySQL
+ Source Server Version : 80016
+ Source Host           : localhost:3306
+ Source Schema         : clouddb03
+
+ Target Server Type    : MySQL
+ Target Server Version : 80016
+ File Encoding         : 65001
+
+ Date: 26/07/2019 16:48:48
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for dept
+-- ----------------------------
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept`  (
+  `deptno` bigint(20) NOT NULL AUTO_INCREMENT,
+  `dname` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `db_source` varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`deptno`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+INSERT INTO `dept` VALUES (1, '人事部', 'clouddb03');
+INSERT INTO `dept` VALUES (2, '开发部', 'clouddb03');
+INSERT INTO `dept` VALUES (3, '财务部', 'clouddb03');
+INSERT INTO `dept` VALUES (4, '市场部', 'clouddb03');
+INSERT INTO `dept` VALUES (5, '运维部', 'clouddb03');
+
+SET FOREIGN_KEY_CHECKS = 1;
